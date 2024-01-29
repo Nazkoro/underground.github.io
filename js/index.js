322edb9c0b95c7f3ea17652d4075b5cb3c4ee327
Nazkoro <rusetskiy2788@mail.ru>
2024-01-29 22:13:35 +0300

    feat: allow only 3 request for person

--- js/index.js
+++ js/index.js
@@ -2,18 +2,25 @@ const TELEGRAM_BOT_TOKEN = '6974955523:AAE1azHgrnSl8Pdb2qstwpPU2LIGc1Ra3KM';
 const TELEGRAM_CHAT_ID = '-1001899112708';
 const textAPI = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
 const photoAPI = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`
+let requestCounter = 0
 
 async function sendEmailTelegram(event) {
-    event.preventDefault();
-
+     event.preventDefault();
+     if(requestCounter >= 2){
+        alert('Внимание! Вы отправили уже 3 заявки. Для защиты от спама форма будет заблокирована на 1 час')
+        document.getElementById("send-btn").disabled = true; 
+        setTimeout(() => {
+            requestCounter = 0;
+            document.getElementById("send-btn").disabled = false;
+        }, 1000 * 60 *60);
+        return
+     }
     const form = event.target;
     const formBtn = document.querySelector('.form__submit-button button')
     const formSendResult = document.querySelector('.form__send-result')
     formSendResult.textContent = '';
 
-
     const { fio, phone, year, height, weight, sport_type, city , ranks, insta ,awards , hasExpirience , telega } = Object.fromEntries(new FormData(form).entries());
-    
     const text = `Заявка от ${fio}!\nТелефон: ${phone}\nВозраст: ${year}\nРост: ${height} см\nВес: ${weight} кг\nВид спорта: ${sport_type}\nГород: ${city}\nРазряды: ${ranks}\nИнстаграм: ${insta}\nНаграды: ${awards}\nПроекты: ${hasExpirience}\nТелеграмм: ${telega}`;
 
     const fileInput = document.getElementById('filefield');
@@ -47,7 +54,9 @@ async function sendEmailTelegram(event) {
         ]) 
         
         if (response1.ok && response2.ok) {
-            formSendResult.textContent = 'Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.';
+            requestCounter+=1
+            alert('Ваша заявка отправлена, мы с вами свяжемся!')
+            formSendResult.textContent = 'Ваша заявка отправлена, мы с вами свяжемся!';
             form.reset()
         } else {
             throw new Error('Ошибка выполнения запроса');
